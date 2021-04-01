d7ae56b6476dc237a1e6b94297628803cafa9734
glenchiridza <glenchiridz@gmail.com>
2021-04-01 21:35:53 +0200

    fragment add content

--- app/src/main/java/com/glencconnnect/net1/models/Contacts.java
+++ app/src/main/java/com/glencconnnect/net1/models/Contacts.java
@@ -5,6 +5,113 @@
 package com.glencconnnect.net1.models;
 
 public class Contacts {
+
+    private static String[] offices = {
+
+            "Head Office",
+            "Beitbridge Shop",
+            "Borrowdale Shop",
+            "Bulawayo Shop",
+            "Bulawayo Airport",
+            "Bulawayo Main Shop",
+            "Chinhoyi Shop",
+            "Chiredzi Shop",
+            "Chitungwiza Shop",
+            "Chivhu Shop",
+            "Eastgate Shop",
+            "Gokwe Shop",
+            "Herbert Chitepo Branch",
+            "Gweru Shop",
+            "Julius Nyerere Shop",
+            "Kamfisa Shop",
+            "Kopje Plaza Shop",
+            "Kwekwe Shop",
+            "Longchen Shop",
+            "Machipisa Shop",
+            "Marondera Shop",
+            "Masvingo Shop",
+            "Mataga Shop",
+            "Mutare Shop",
+            "Ramjis Shop","Robert Mugabe International Airport", "Rusape Shop", "Vangaurd Shop", "Victoria Falls Shop","WestGate Shop","ZITF Shop",
+            "Zvishavane Shop"
+    };
+
+
+    public static String[] addresses = {
+
+            "Ascent Link Communications, 1672 Gokwe Centre",
+            "344 Herbert Chitepo, (Between 9th and Enterprise road), Harare",
+            "Corner Rober Mugabe and 7th street, Gweru",
+            "Cnr J.Nyerere // Nelson Mandela, Fidelity House, Ground Floor, Harare",
+            "17 Grorge Square, Kamfinsa Shopping Center, Harare",
+            "1 Jason Moyo Avenue, Kopje Plaza Building, Harare",
+            "Shop 4 CAIPF Building, Robert Mugabe Road, Kwekwe",
+            "Shop 79/80(A1/18), Cnr Samora/Mutley Bend, Belvedere, Harare",
+            "985 J ababvu Street, Machipisa, Harare",
+            "2 Cnr Ruzawi/Pine Street, Marondera",
+            "ZESA Building , Robert Mugabe Way",
+            "Mabhena Enterprises Stand 81, Mataga, Mberengwa",
+            "93-94 Herbert Chitepo, Fidelity Life Tower",
+            "Stand 2 Ramjis Complex, Bulawayo",
+            "Robert Mugabe International Airport, International Arrivals, Harare",
+            "Stand 13, Cnr R.Mugabe and H.Chitepo, Rusape",
+            "Cnr 4th/ J.Moyo, Vangaurd House, Harare",
+            "19 Galleris Building, Victoria Falls",
+            "No. 56, WestGate Shopping Mall, Harare",
+            "Netone, Pavillion Kudu Cresent, ZITF Grounds, Bulawayo",
+            "998 Ireland Road, Zvishavane"
+    };
+
+    public static String[] phone_num = {
+            " ",
+            " ",
+            "054 223500/501 ",
+            " 0242 772039",
+            "0242 443488/495255",
+            "0242 771764",
+            "055 25303/4",
+            " ",
+            " ",
+            " ",
+            "039 264543",
+            "0712980855",
+            "020 65336",
+            "020 60555/60539/60522",
+            " ",
+            "025 5205/2241",
+            " ",
+            "083 41049/51",
+            "0242 335960/66",
+            " ",
+            "039 2209/3916"
+
+    };
+
+    public static String[] emails = {
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " ",
+            " "
+
+    };
+
 }
 
 
